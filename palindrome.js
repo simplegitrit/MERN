a70858436d03fe38da5362696a5ef49f13623c9d
simplegitrit <127583449+simplegitrit@users.noreply.github.com>
2023-12-23 22:37:13 +0530

    Add files via upload

---
+++ palindrome.js
@@ -0,0 +1,17 @@
+/*
+  Implement a function `isPalindrome` which takes a string as argument and returns true/false as its result.
+  Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
+*/
+
+function isPalindrome(str) {
+  let j = str.length - 1;
+  for(let i = 0 ; i < str.length/2 ; i++){
+    if(str[i] != str[j]){
+      return false;
+    }
+    j--;
+  }
+  return true;
+}
+
+module.exports = isPalindrome;

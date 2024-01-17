90b7f9c0b97058f26c6a71697d6d9524119d6e36
simplegitrit <127583449+simplegitrit@users.noreply.github.com>
2024-01-17 18:42:27 +0530

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const adminMiddleware = require("../middleware/admin");
+const {Admin, Course} = require('../db')
+const router = express.Router();
+
+// Admin Routes
+router.post('/signup', async(req, res) => {
+    // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    await Admin.create({
+        username: username,
+        password: password
+    })
+    res.json({
+        message: "Admin Created Successfully"
+    })
+});
+
+router.post('/courses', adminMiddleware, async(req, res) => {
+    // Implement course creation logic
+    const title = req.body.title;
+    const description = req.body.description;
+    const imageLink = req.body.imageLink;
+    const price = req.body.price;
+
+    const newCourse = await Course.create({
+        title,
+        description,
+        imageLink,
+        price
+    })
+    console.log(newCourse);
+    res.json({
+        message: "Course created successfully",
+        courseId: newCourse._id
+    })
+});
+
+router.get('/courses', adminMiddleware, (req, res) => {
+    // Implement fetching all courses logic
+});
+
+module.exports = router;
